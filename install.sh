1d1d76b13413d507b77202deabbcbfe42d3c6c92
buggsboony <bruno.lesdema@gmail.com>
2021-03-11 12:24:46 -0400

    add restart cinnamon

--- install.sh
+++ install.sh
@@ -2,11 +2,20 @@
 
 #install stuff
 what=${PWD##*/}   
+what2=cinnamon
 extension=.sh
 #peut être extension vide
 
-echo "Set executable..."
+echo "Set executable $what..."
 chmod +x $what$extension
 #echo "lien symbolique vers usr bin"
 sudo ln -s "$PWD/$what$extension" /usr/bin/$what
+echo "done."
+
+
+
+echo "Set executable $what2..."
+chmod +x $what2$extension
+#echo "lien symbolique vers usr bin"
+sudo ln -s "$PWD/$what2$extension" /usr/bin/$what2
 echo "done."
